0414546ef538f0a8fce2693aa26b80ac8e7efa8f
diegoliber <diegoliber@gmail.com>
2015-04-18 09:18:51 -0300

    Adding the new version of wildfly-cli-wrapper with a more sofisticated way of handling command line parameters using JCommander. The exec part in util/cli.pp now contains parameters for connecting to a jboss located in a different address and different port. Before that, it could only connect to 'localhost:9999'.

--- manifests/util/cli.pp
+++ manifests/util/cli.pp
@@ -1,14 +1,15 @@
 #
 # Uses Wildfly CLI Wrapper to ensure configuration state
 #
-define wildfly::util::cli($content = undef, $path = undef) {
+define wildfly::util::cli($content = undef, $path = undef, $address = 'localhost', $port = '9999') {
 
   $cleaned_content = delete_undef_values($content)
   $json_content = to_unescaped_json($cleaned_content)
 
+
   exec { $title:
-    command => "java -jar ${wildfly::dirname}/bin/client/wildfly-cli-wrapper.jar \"${path}\" ${json_content}",
-    unless  => "java -jar ${wildfly::dirname}/bin/client/wildfly-cli-wrapper.jar \"${path}\" ${json_content} --verify-only",
+    command => "java -jar ${wildfly::dirname}/bin/client/wildfly-cli-wrapper.jar --path \"${path}\" --content ${json_content} --target-address ${address} --target-port ${port}",
+    unless  => "java -jar ${wildfly::dirname}/bin/client/wildfly-cli-wrapper.jar --path \"${path}\" --content ${json_content} --target-address ${address} --target-port ${port} --verify-only",
     path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin', "${wildfly::java_home}/bin"],
     require => Service['wildfly']
   }
