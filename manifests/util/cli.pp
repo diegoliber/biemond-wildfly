52f773354aaedf2c1efe5fa3080f8d3906907c64
diegoliber <diegoliber@gmail.com>
2015-04-18 12:59:47 -0300

    Removing --target-port from jboss-cli-wrapper call

--- manifests/util/cli.pp
+++ manifests/util/cli.pp
@@ -1,15 +1,15 @@
 #
 # Uses Wildfly CLI Wrapper to ensure configuration state
 #
-define wildfly::util::cli($content = undef, $path = undef, $address = $wildfly::mgmt_bind, $port = 9999) {
+define wildfly::util::cli($content = undef, $path = undef, $address = $wildfly::mgmt_bind) {
 
   $cleaned_content = delete_undef_values($content)
   $json_content = to_unescaped_json($cleaned_content)
 
 
   exec { $title:
-    command => "java -jar ${wildfly::dirname}/bin/client/wildfly-cli-wrapper.jar --path \"${path}\" --content ${json_content} --target-address ${address} --target-port ${port}",
-    unless  => "java -jar ${wildfly::dirname}/bin/client/wildfly-cli-wrapper.jar --path \"${path}\" --content ${json_content} --target-address ${address} --target-port ${port} --verify-only",
+    command => "java -jar ${wildfly::dirname}/bin/client/wildfly-cli-wrapper.jar --path \"${path}\" --content ${json_content} --target-address ${address}",
+    unless  => "java -jar ${wildfly::dirname}/bin/client/wildfly-cli-wrapper.jar --path \"${path}\" --content ${json_content} --target-address ${address} --verify-only",
     path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin', "${wildfly::java_home}/bin"],
     require => Service['wildfly']
   }
